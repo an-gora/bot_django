20b7bbb143514bdbc62bdfa086345b3905dcb906
Ana <an.gordievich@gmail.com>
2022-05-27 23:40:17 +0300

    add

--- bot_django/settings.py
+++ bot_django/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'olx_bot_app',
-    'olx_parser'
+    'olx_parser',
+    't_bot',
 ]
 
 MIDDLEWARE = [
--- t_bot/states/hello.py
+++ t_bot/states/hello.py
@@ -1,3 +1,4 @@
+import export as export
 from telebot import types
 
 from olx_bot_app.models import Subscriber, Ad
@@ -5,18 +6,15 @@ from t_bot.states.base import BaseState
 
 
 class Hello(BaseState):
-    text = "Привет! Я помогу тебе найти квартиру в аренду на сайте OLX. Введи название города в Украине"
+    text = "Привет! Я помогу тебе найти квартиру в аренду в Украине. Введи название города."
 
-    def save_subscribe(self):
-        # subscriber = Subscriber.objects.get_or_create(chat_id=self.chat_id)
-        # last_apartment = Ad.objects.latest('id').url
-        # self.bot.send_message(self.chat_id, last_apartment)
-        self.bot.send_message(self.chat_id, 'test')
-
+    # def save_subscriber(self):
+    #     subscriber = Subscriber.objects.get_or_create(chat_id=self.chat_id)
+    #     last_sended_ad = Ad.objects.filter('city').latest('id').url
+    #     self.bot.send_message(self.chat_id, last_sended_ad)
 
     def process_call_back(self, message: types.CallbackQuery):
         if message.data and message.data == 'nextstate:ActionState':
-            self.save_subscribe()
             return ActionState
         return Hello
 
@@ -27,73 +25,24 @@ class Hello(BaseState):
 
 
 class ActionState(BaseState):
-    text = "выбрать 1 2 3 "
+    text = "Вітаю, у нас діє акції! Оберіть: 1 - Новий рік, 2 - Багато хвилин, 3 - Скоро повернемо"
 
     def process_text_message(self, message: types.Message):
         if message.text in ('1', '2', '3'):
-            return SubscribedState
+            return ActionAppliedState
         self.send_warning('Натисніть 1, 2 або 3!')
         return ActionState
 
 
-# class ActionAppliedState(BaseState):
-#     text = "Вибачте, всі оператори зайняті!"
-#
-#     def get_keyboard(self):
-#         keyboard = types.InlineKeyboardMarkup()
-#         keyboard.add(types.InlineKeyboardButton(text='Додому', callback_data='nextstate:Hello'))
-#         return keyboard
-#
-#     def process_call_back(self, message: types.CallbackQuery):
-#         if message.data and message.data == 'nextstate:Hello':
-#             return Hello
-#         return ActionAppliedState
-
-
-class SubscribedState(BaseState):
-    text = 'Вы успешно подписались на новые обьявления о сдаче квартир'
+class ActionAppliedState(BaseState):
+    text = "Вибачте, всі оператори зайняті!"
 
     def get_keyboard(self):
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(
-            types.InlineKeyboardButton(text='Отменить подписку', callback_data='nextstate:Hello'))
+        keyboard.add(types.InlineKeyboardButton(text='Додому', callback_data='nextstate:Hello'))
         return keyboard
 
-# from telebot import types
-#
-# from olx_bot_app.models import Ad, Subscriber
-# from t_bot.states.base import BaseState
-#
-#
-# class Hello(BaseState):
-#     text = "Привет! Я помогу тебе найти квартиру в аренду в Украине. Введи название города"
-#
-#     def save_subscribe(self):
-#         subscriber = Subscriber.objects.get_or_create(chat_id=self.chat_id)
-#         last_apartment = Ad.objects.latest('id').url
-#         self.bot.send_message(self.chat_id, last_apartment)
-#
-#         # last_news = News.objects.latest('date').link
-#         # self.bot.send_message(self.chat_id, last_news)
-#
-#     def process_call_back(self, message: types.CallbackQuery):
-#         if message.data and message.data == 'nextstate:SubscribedState':
-#             self.save_subscribe()
-#             return SubscribedState
-#         return Hello
-#
-#     def get_keyboard(self):
-#         keyboard = types.InlineKeyboardMarkup()
-#         keyboard.add(
-#             types.InlineKeyboardButton(text='Пiдписатися на всi новини', callback_data='nextstate:SubscribedState'))
-#         return keyboard
-#
-#
-# class SubscribedState(BaseState):
-#     text = 'Вы успешно подписались на новые обьявления о сдаче квартир'
-#
-#     def get_keyboard(self):
-#         keyboard = types.InlineKeyboardMarkup()
-#         keyboard.add(
-#             types.InlineKeyboardButton(text='Отменить подписку', callback_data='nextstate:Hello'))
-#         return keyboard
+    def process_call_back(self, message: types.CallbackQuery):
+        if message.data and message.data == 'nextstate:Hello':
+            return Hello
+        return ActionAppliedState
